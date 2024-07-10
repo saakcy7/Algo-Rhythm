17e61ea84f3ee39bd4d46456e3cdb958926163bf
Sakshi KC <kcsakshi92@gmail.com>
2024-07-10 18:48:39 +0545

    created required folders

---
+++ algorithm/src/data.js
@@ -0,0 +1,19 @@
+// src/data.js
+export const algorithms = [
+    {
+        name: 'Binary Search',
+        description: 'A search algorithm that finds the position of a target value within a sorted array.',
+        image: require('./images/binary-search.png')
+    },
+    {
+        name: 'Merge Sort',
+        description: 'A divide-and-conquer algorithm that was invented by John von Neumann in 1945.',
+        image: require('./images/merge-sort.png')
+    },
+    {
+        name: 'Quick Sort',
+        description: 'An efficient sorting algorithm, serving as a systematic method for placing the elements of a list in order.',
+        image: require('./images/quick-sort.png')
+    },
+    // Add more algorithms as needed
+];
