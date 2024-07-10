5d8c67d56319838bc9bb90c9265200fd53b866a7
Sakshi KC <kcsakshi92@gmail.com>
2024-07-10 18:52:14 +0545

    card for displaying content

---
+++ algorithm/src/components/AlgorithmCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './AlgorithmCard.css';
+
+const AlgorithmCard = ({ name,description,image }) => {
+return(
+    <div className="algorithm-card">
+        <img src={image} alt={`${name}`} className="algorithm-image" />
+        <h2>{name}</h2>
+        <p>{description}</p>
+    </div>
+)
+}
+export default AlgorithmCard;
