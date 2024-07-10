3c7e0bb4df2410f8df10ce0a88d2555ae75a0298
Sakshi KC <kcsakshi92@gmail.com>
2024-07-10 19:39:23 +0545

    routing fixed

--- algorithm/src/App.js
+++ algorithm/src/App.js
@@ -1,25 +1,24 @@
 // src/App.js
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter } from 'react-router-dom';
-import { Route,Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route,Routes } from 'react-router-dom';
 import Home from '../src/pages/home';
 import About from '../src/pages/about';
 import Algorithms from '../src/pages/algorithms';
 const App = () => {
     return (
+      <Router>
         <div className="App">
           <Navbar/>
-            <header className="App-header">
-                <h1>DSA Popular Algorithms</h1>
-            </header>
-            <Router>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/about" component={About} />
-                    <Route path="/algorithms" component={Algorithms} />
-                    </Router>    
+            <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/about" element={<About/>} />
+                    <Route path="/algorithms" element={<Algorithms/>} />
+            </Routes>    
               
         </div>
+        </Router>
     );
 };
 
--- algorithm/src/pages/algorithms.js
+++ algorithm/src/pages/algorithms.js
@@ -1,11 +1,9 @@
 import React from "react";
 import AlgorithmList from '../components/AlgorithmList';
 import { algorithms } from '../data';
-import Navbar from '../components/Navbar';
 const Algorithms = () => {
     return (
         <div>
-            <Navbar/>
             <h1>Popular Algorithms</h1>
             <AlgorithmList algorithms={algorithms} />
         </div>
